b7b52b4f8178fabba0e8f3ad4ce48d98912d907c
Zak El Fassi <zakelfassi@gmail.com>
2024-12-17 18:52:08 -0800

    bump

--- dist-electron/main.js
+++ dist-electron/main.js
@@ -1,34 +1,24 @@
-import { app, BrowserWindow } from "electron";
-import { fileURLToPath } from "url";
-import path from "path";
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-function createWindow() {
-  const win = new BrowserWindow({
+import { app as e, BrowserWindow as a } from "electron";
+import { fileURLToPath as l } from "url";
+import o from "path";
+const s = l(import.meta.url), n = o.dirname(s);
+function i() {
+  const t = new a({
     width: 1200,
     height: 800,
     webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false,
-      preload: path.join(__dirname, "preload.js")
+      nodeIntegration: !0,
+      contextIsolation: !1,
+      preload: o.join(n, "preload.js")
     }
-  });
-  const startURL = app.isPackaged ? `file://${path.join(__dirname, "../dist/index.html")}` : "http://localhost:3000";
-  win.loadURL(startURL);
-  if (!app.isPackaged) {
-    win.webContents.openDevTools();
-  }
+  }), r = e.isPackaged ? `file://${o.join(n, "../dist/index.html")}` : "http://localhost:3000";
+  t.loadURL(r), e.isPackaged || t.webContents.openDevTools();
 }
-app.whenReady().then(() => {
-  createWindow();
-  app.on("activate", () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
-    }
+e.whenReady().then(() => {
+  i(), e.on("activate", () => {
+    a.getAllWindows().length === 0 && i();
   });
 });
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
+e.on("window-all-closed", () => {
+  process.platform !== "darwin" && e.quit();
 });
